d8065c02d1a29305b122735110e9ffb3376649d7
Rikesh Makwana <rikesh.makwana@gmail.com>
2022-10-20 20:32:46 +0400

    Install react icon library

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,37 @@ import React from 'react';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import ProblemSpace from './components/ProblemSpace';
 import Introduction from './components/Introduction';
+import Headline from './components/Headline';
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <ProblemSpace />
-        <Introduction />
+        {/* <ProblemSpace /> */}
+        {/* <Introduction /> */}
+
+        <div className="our-solution fs-5">
+          <Row>
+            <Col md="6">
+              <Headline headline="AI and ML Developers"/>
+              <p>By providing access to computing which is</p>
+              <ul>
+                <li>Affordable</li>
+                <li>Time-Efficient</li>
+                <li>Optimized</li>
+              </ul>
+              <p>alongside facilitating easy development with features like</p>
+              <ul>
+                <li>Distributed Learning</li>
+                <li>One-Step Plugins</li>
+              </ul>
+            </Col>
+            <Col md="6">Image Here</Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
